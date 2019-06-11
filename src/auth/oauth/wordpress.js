093f9d1384dc05625441d9982893f3bc93bb3b8d
Matt Wilkin <mwilkin22@gmail.com>
2019-06-11 14:45:51 -0700

    wordpress auth creation and initiate auth process start.

---
+++ src/auth/oauth/wordpress.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const superagent = require('superagent');
+const Users = require('../users-model.js');
+
+const API = 'http://localhost:3000';
+const WPTS = 'https://public-api.wordpress.com/oauth2/token';
+const SERVICE = 'https://public-api.wordpress.com/oauth2/authorize';
+
+// const AUTHORIZE_URL='https://public-api.wordpress.com/oauth2/authorize';
+// const AUTHENTICATE_URL='https://public-api.wordpress.com/oauth2/authenticate';
+
+
+let authorize = (request) => {
+  
+  console.log('(1)', request.query.code);
+  
+  return superagent.post(WPTS)
+    .type('form')
+    .send({
+      code: request.query.code,
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `${API}/oauth`,
+      grant_type: 'authorization_code',
+    })
+    .then( response => {
+      let access_token = response.body.access_token;
+      console.log('(2)', access_token);
+      return access_token;
+    })
+    .then(token => {
+      console.log(SERVICE, token);
+      return superagent.get(SERVICE)
+        .set('Authorization', `Bearer ${token}`)
+        .then( response => {
+          let user = response.body;
+          console.log('(3)', user);
+          return user;
+        });
+    })
+    .then( oauthUser => {
+      console.log('(4) Create Our Account');
+      return Users.createFromOauth(oauthUser.email);
+    })
+    .then( actualUser => {
+      return actualUser.generateToken(); 
+    })
+    .catch( error => error );
+};
+
+
+module.exports = authorize;
